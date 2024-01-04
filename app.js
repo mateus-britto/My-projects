f6348c0903bc46d407424bfc31cf19aa30cf68d9
Mateus Britto <mateusbritto00@gmail.com>
2024-01-04 17:42:06 +0100

    Add changes to the main page
    - Added a call to arms to the intro section
    
    - Changed font family to various parts of the page and other styling
    
    - Added tic-tac-toe project to the work section
    
    - Added tic-tac-toe project to the work section

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const hiddenElements = document.querySelectorAll(".hidden");
+const contactBtn = document.querySelector(".contact-btn");
 
 // The observer checks for changes in the visibility of the selected elements and,
 // anytime it detects a change, adds a class 'show' to them, which can then be manipulated via CSS.
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log("entry");
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
@@ -14,3 +14,8 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Event listener to scroll to contacts section on click
+contactBtn.addEventListener("click", function () {
+  window.scrollTo(0, document.body.scrollHeight);
+});
